d6c29298179b471fb7fbd223c5fb638e24ae0f96
JBC <hello@jeanbaptistecalzia.fr>
2024-09-27 20:51:27 +0200

    Add home-page and about-page classes to body

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,6 +1,14 @@
 import Banner from "../../components/Banner/";
+import { useEffect } from "react";
 
 function About() {
+  useEffect(() => {
+    document.body.classList.add("about-page");
+    return () => {
+      document.body.classList.remove("about-page");
+    };
+  }, []);
+
   return <Banner></Banner>;
 }
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,14 @@
 import Banner from "../../components/Banner/";
+import { useEffect } from "react";
 
 function Home() {
+  useEffect(() => {
+    document.body.classList.add("home-page");
+    return () => {
+      document.body.classList.remove("home-page");
+    };
+  }, []);
+
   return (
     <Banner>
       <h1>Chez vous, partout et ailleurs</h1>

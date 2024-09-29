3c6969555af993e17c93cc0d3600d1499f4c94ed
JBC <hello@jeanbaptistecalzia.fr>
2024-09-29 09:48:55 +0200

    Add error page component

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,7 +1,20 @@
+import "../../styles/error.scss";
+import { Link } from "react-router-dom";
+import { useEffect } from "react";
+
 function Error() {
+  useEffect(() => {
+    document.body.classList.add("error-page");
+    return () => {
+      document.body.classList.remove("error-page");
+    };
+  }, []);
+
   return (
-    <div>
-      <h1>Error 404</h1>
+    <div className="content">
+      <h1>404</h1>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/">Retourner sur la page d’accueil</Link>
     </div>
   );
 }
